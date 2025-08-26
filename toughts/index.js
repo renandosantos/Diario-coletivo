be6121eb67ca965a3aaaa87da5c6a34939b0bf6e
renan rodrigues dos santos <renandosantos.ti@gmail.com>
2025-08-26 19:24:55 -0300

    finalizei a estrutura

--- toughts/index.js
+++ toughts/index.js
@@ -15,7 +15,49 @@ const port = 5000
 // 3. CONFIGURAÇÕES DO APP
 // (Inicialização do App, formulários, pasta 'public' e View Engine)
 const app = express()
+app.use(express.static('public'))
 
+//template engine
+app.engine =('handlebars', engine())
+app.set('viewengine', 'handlebars')
+
+//receber resposta
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
+app.use(express.json())
+
+//session middleware
+app.use(
+    session({
+        name:"session",
+        secret: "nosso_secret",
+        resave: false,
+        saveUninitialized: false,
+        store: new FileStore({
+            logFn: function() {},
+            path: require('path').join(require('os').tmpdir(), 'sessions'),
+        }),
+        cookie: {
+            secure: false,
+            maxAge: 360000,
+            expires:new Date(Date.now() + 360000),
+            httpOnly: true,            
+        }
+    })
+)
+//flash messages 
+app.use(flash())
+
+//set session to res
+app.use((req, res, next) => {
+    if(req.session.userid) {
+        res.locals.session = req.session
+    }
+    next()
+})
 
 // 4. ROTAS
 // (O App "usa" as rotas importadas aqui)
