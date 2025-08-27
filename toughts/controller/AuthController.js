82625d21f34851cee111bcb0744167430005b14e
renan rodrigues dos santos <renandosantos.ti@gmail.com>
2025-08-27 14:36:24 -0300

    flash message

--- toughts/controller/AuthController.js
+++ toughts/controller/AuthController.js
@@ -1,3 +1,7 @@
+const User = require('../models/User')
+
+const bcrypt = require('bcryptjs')
+
 module.exports = class AuthController {
 
     static login(req, res ) {
@@ -6,5 +10,19 @@ module.exports = class AuthController {
 
     static register(req, res ) {
         res.render('auth/register')
+    }
+
+    static async registerPost(req, res) {
+
+        const { name, email, password, confirmpasword } = req.body
+
+        //password match validation
+        if(password != confirmpasword) {
+            req.flash('message', 'As senhas não conferem, tente novamente!')
+            res.render('auth/register')
+
+            return
+        }
+
     }
 }
