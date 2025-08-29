cd76317351447fd0a4489d21fcbba3a8d3da4fef
renan rodrigues dos santos <renandosantos.ti@gmail.com>
2025-08-29 09:40:01 -0300

    mensagem vazia

--- toughts/controller/ToughtsController.js
+++ toughts/controller/ToughtsController.js
@@ -25,9 +25,13 @@ module.exports = class ToughtController {
 
         const toughts = user.Toughts.map((result) => result.dataValues)
 
+        let emptyToughts = false
 
+        if(toughts.length === 0 ) {
+            emptyToughts = true
+        } 
 
-        res.render('toughts/dashboard', { toughts })
+        res.render('toughts/dashboard', { toughts, emptyToughts })
     }
 
     static createTought(req, res) {
